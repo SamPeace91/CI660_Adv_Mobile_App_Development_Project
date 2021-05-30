da60968d1368cd9de6f7649494b5b57d9345e24f
SamPeace91 <sampeace91@yahoo.co.uk>
2021-05-30 16:53:55 +0100

    DontDestroyOnLoad Extra Changes
    More tweaks.

--- Assets/Scripts/DontDestroyOnLoad.cs
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -12,10 +12,6 @@ public class DontDestroyOnLoad : MonoBehaviour
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         GameObject[] controls = GameObject.FindGameObjectsWithTag("Controls");
         GameObject[] menuCanvas = GameObject.FindGameObjectsWithTag("Menu");
-        //GameObject[] settingsCanvas = GameObject.FindGameObjectsWithTag("Settings");
-        //GameObject[] pauseCanvas = GameObject.FindGameObjectsWithTag("Pause");
-        //GameObject[] winCanvas = GameObject.FindGameObjectsWithTag("Win");
-        //GameObject[] loseCanvas = GameObject.FindGameObjectsWithTag("Lose");
 
         // If a 'SceneManager' already exists (from picking a new level in the win screen) then this new 'SceneManager' is destroyed
         if (sceneManagers.Length > 1 || huds.Length > 1 || players.Length > 1 || controls.Length > 1 || menuCanvas.Length > 1) //|| settingsCanvas.Length > 1 || pauseCanvas.Length > 1 || winCanvas.Length > 1 || loseCanvas.Length > 1)
